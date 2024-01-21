75b0948265897ef7e15af51a06a58016477ef22c
DHCPCD <dhcpcd9@gmail.com>
2024-01-21 18:01:09 +0100

    feat(downloads): Prevent beatmap redownloading unless it actually updated

--- src/api/downloads/mod.rs
+++ src/api/downloads/mod.rs
@@ -1,20 +1,95 @@
 use std::{sync::Arc, path::Path as path_sys};
 
-use axum::{extract::Path, Extension, Router, routing::get, response::{Response}, body::Body};
+use axum::{extract::Path, Extension, Router, routing::get, response::Response, body::Body};
+use chrono::{DateTime, Local};
 use serde_json::json;
 use tokio::io::AsyncReadExt;
-use tracing::error;
+use tracing::{error, info};
 
-use crate::{crawler::Context, osu::client::OsuApi};
+use crate::{crawler::Context, osu::client::OsuApi, ops::{beatmapset::get_beatmapset_by_id, DownloadIndex}};
+
+async fn create_new_index(ctx: Arc<Context>, id: i64) -> Option<DownloadIndex> {
+    let mut download_index: Option<DownloadIndex> = None;
+    download_index = Some(DownloadIndex { id: id, date: Local::now().timestamp()});
+    if let Err(err) = ctx.meili_client.index("downloads").add_documents(&[download_index.clone().unwrap()], Some("id")).await {
+        error!("Failed to create index: {}", err);
+    }else{
+        info!("Created new index: {:#?}", download_index);
+    }
+
+    download_index
+}
+
+async fn get_index_or_create(ctx: Arc<Context>, id: i64) -> Option<DownloadIndex> {
+    let index_response = ctx.meili_client
+    .index("downloads")
+    .search()
+    .with_filter(format!("id = {}", id).as_str())
+    .execute::<DownloadIndex>()
+    .await;
+    
 
 
+    if let Err(err) = index_response {
+        //Index not found, creating inserting index
+        let download_index = create_new_index(ctx.clone(), id).await;
+        error!("Failed to get index: {}, created new one: {:#?}", err, download_index);
+
+        return download_index
+    }
+
+    let index = index_response.unwrap();
+
+    if index.total_hits.unwrap_or(0) < 1 {
+        //Creating new index
+        let download_index = create_new_index(ctx.clone(), id).await;
+
+        return download_index;
+    }
+
+    //Unpacking first index and putting it in download_index
+    if let Some(index) = index.hits.first() {
+        return Some(index.result.clone());
+    }
+
+    None
+}
 
 async fn download(
     Extension(ctx): Extension<Arc<Context>>,
-    Path(id): Path<String>
+    Path(id): Path<i64>
 ) -> Response {
 
-    if ctx.osu.download_if_not_exists(id.clone(), ctx.config.clone().beatmaps_folder.clone()).await.is_err() {
+    let mut redownload_required = false;
+
+    let index = get_index_or_create(ctx.clone(), id).await;
+    if index.is_none() {
+        Response::builder().status(500).body(Body::from(json!({"ok": false, "message": "Internal database exception"}).to_string())).unwrap();
+    }
+   
+    let beatmapset = get_beatmapset_by_id(ctx.clone(), id).await;
+
+    match beatmapset {
+        Ok(set) => {
+            //parsing time from last_updated field
+            let date = DateTime::parse_from_rfc3339(&set.last_updated);
+            if date.is_err() {
+                return Response::builder().body(Body::from(json!({"ok": false, "message": "Failed to parse date"}).to_string())).unwrap();
+            }
+            let date = date.unwrap();
+            let last_updated = date.timestamp();
+
+            //if last updated is bigger than last download date
+            if last_updated > index.unwrap().date {
+                info!("Redownloading {}, it is too old", id);
+
+                redownload_required = true;
+            }
+        }
+        _ => {}
+    }
+
+    if ctx.osu.download_if_not_exists(id.clone(), ctx.config.clone().beatmaps_folder.clone(), redownload_required).await.is_err() {
         return Response::builder().body(Body::from(json!({"ok": false, "message": "Failed to download file"}).to_string())).unwrap();
     }
 
@@ -30,9 +105,16 @@ async fn download(
     let mut content = Vec::new();
     file.read_to_end(&mut content).await.unwrap();
     
+    let mut file_name = format!("{}.osz", id);    
+    let beatmapset = get_beatmapset_by_id(ctx, id).await;
+    
+    if let Ok(beatmapset) = beatmapset {
+        file_name = format!("{} {} - {}.osz", beatmapset.id, beatmapset.artist, beatmapset.title);
+    }
+
     Response::builder()
     .header("Content-Type", "application/x-osu-beatmap-archive")
-    .header("Content-Disposition", format!("attachment; filename={}.osz", id))
+    .header("Content-Disposition", format!("attachment; filename={}", file_name))
     .body(Body::from(content))
     .unwrap()    
 }
--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,2 +1,12 @@
+
+
+use serde_derive::{Deserialize, Serialize};
+
 pub mod beatmaps;
 pub mod beatmapset;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct DownloadIndex {
+    pub id: i64,
+    pub date: i64
+}
