95dc9e9bc002bb85c1e6f998ce1f42d9d04224ac
DHCPCD <dhcpcd9@gmail.com>
2024-01-21 15:22:04 +0100

    feat(loggining): Added loggining for requests

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,29 +1,32 @@
-pub mod beatmapsets;
 pub mod beatmaps;
+pub mod beatmapsets;
 pub mod downloads;
 pub mod search;
 
 use std::sync::Arc;
 
-use axum::{Router, Extension};
+use axum::{Extension, Router};
 use tower::ServiceBuilder;
-
+use tower_http::trace::{DefaultOnRequest, TraceLayer, DefaultOnResponse};
+use tracing::Level;
 
 use crate::crawler::Context;
-
 
 pub async fn serve(ctx: Context) {
     let arc_ctx = Arc::new(ctx);
 
     let layer_ctx = ServiceBuilder::new()
-    .layer(Extension(arc_ctx.clone()));
+        .layer(TraceLayer::new_for_http()
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO).latency_unit(tower_http::LatencyUnit::Millis)))
+        .layer(Extension(arc_ctx.clone()));
 
     let router = Router::new()
-    .merge(crate::api::beatmapsets::serve())
-    .merge(crate::api::beatmaps::serve())
-    .merge(crate::api::downloads::serve())
-    .merge(crate::api::search::serve())
-    .layer(layer_ctx);
+        .merge(crate::api::beatmapsets::serve())
+        .merge(crate::api::beatmaps::serve())
+        .merge(crate::api::downloads::serve())
+        .merge(crate::api::search::serve())
+        .layer(layer_ctx);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
