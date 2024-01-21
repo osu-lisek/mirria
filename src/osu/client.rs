a6ff640d6ed4955884bb15268a2460895d0b2eea
DHCPCD <dhcpcd9@gmail.com>
2024-01-21 15:24:10 +0100

    feat(client): Handle invalid tokens and validate tokens before use.

--- src/osu/client.rs
+++ src/osu/client.rs
@@ -1,13 +1,19 @@
-use std::{future::{Future, self}, io::{Error, ErrorKind}, time::{UNIX_EPOCH, SystemTime}, borrow::BorrowMut, fs::write, path::Path};
+use std::{
+    borrow::BorrowMut,
+    fs::write,
+    io::{Error, ErrorKind},
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 
 use serde_derive::Deserialize;
 use tokio::fs::File;
-use tracing::{info, error};
+use tracing::{error, info};
 
 use crate::config::Configuration;
 
 use super::types::SearchResponse;
-
 
 #[derive(Debug, Clone)]
 pub struct OsuClient {
@@ -17,26 +23,48 @@ pub struct OsuClient {
 }
 
 #[derive(Debug, Deserialize)]
-struct TokenResponse {
+pub struct TokenResponse {
     pub access_token: String,
     pub expires_in: u64,
     pub refresh_token: String,
 }
 
-pub trait OsuApi {
-    async fn from_credentials(config: Configuration, username: String, password: String) -> impl Future<Output = Result<OsuClient, Error>>;    
-    async fn from_tokens(config: Configuration, access_token: String, refresh_token: String) -> OsuClient;
-    async fn refresh_token(&mut self, config: Configuration) -> Result<bool, Error>;
-    async fn search_beatmapsets(&self, nsfw: bool, sort: String, status: String, cursor_string: Option<String>) -> Option<SearchResponse>;
-    async fn download_if_not_exists(&self, id: String, path_to_beatmaps: String) -> Result<Vec<u8>, Error>;
+#[derive(Debug, Deserialize)]
+pub struct UserResponse {
+    pub username: String,
+    pub id: i32,
 }
 
-impl OsuApi for OsuClient {
-    async fn from_credentials(config: Configuration, username: String, password: String) -> impl Future<Output = Result<OsuClient, Error>> {
-        
-        let client = reqwest::Client::new();
-        
-        let form = reqwest::multipart::Form::new()
+pub trait OsuApi {
+    async fn from_tokens(
+        config: Configuration,
+        access_token: String,
+        refresh_token: String,
+    ) -> Result<OsuClient, Error>;
+    async fn refresh_token(&mut self, config: Configuration) -> Result<bool, Error>;
+    async fn search_beatmapsets(
+        &self,
+        nsfw: bool,
+        sort: String,
+        status: String,
+        cursor_string: Option<String>,
+    ) -> Option<SearchResponse>;
+    async fn download_if_not_exists(
+        &self,
+        id: String,
+        path_to_beatmaps: String,
+    ) -> Result<Vec<u8>, Error>;
+    async fn fetch_user(&self) -> Result<UserResponse, Error>;
+}
+
+pub async fn log_in_using_credentials(
+    config: Configuration,
+    username: String,
+    password: String,
+) -> Result<TokenResponse, Error> {
+    let client = reqwest::Client::new();
+
+    let form = reqwest::multipart::Form::new()
         .text("grant_type", "password")
         .text("username", username)
         .text("password", password)
@@ -44,61 +72,126 @@ impl OsuApi for OsuClient {
         .text("client_secret", "FGc9GAtyHzeQDshWP5Ah7dega8hJACAJpQtw6OXk")
         .text("scope", "*");
 
+    let response = client
+        .post("https://osu.ppy.sh/oauth/token")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .header("Accept", "application/json")
+        .multipart(form)
+        .send()
+        .await
+        .unwrap();
+
+    let resp = response.json::<TokenResponse>().await.unwrap();
+
+    let mut new_config = config.clone().borrow_mut().to_owned();
+    new_config.osu_access_token = resp.access_token.clone();
+    new_config.osu_refresh_token = resp.refresh_token.clone();
+    new_config.osu_token_expires_at = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        + resp.expires_in;
+
+    confy::store("mirria", None, new_config).expect("Error while saving config.");
+
+    Ok(resp)
+}
+
+impl OsuApi for OsuClient {
+    async fn fetch_user(&self) -> Result<UserResponse, Error> {
+        let client = reqwest::Client::new();
+
         let response = client
-            .post("https://osu.ppy.sh/oauth/token")
-            .header("Content-Type", "application/x-www-form-urlencoded")
+            .get("https://osu.ppy.sh/api/v2/me")
             .header("Accept", "application/json")
-            .multipart(form)
+            .bearer_auth(self.clone().access_token)
             .send()
-            .await
-            .unwrap();
+            .await;
 
-        let resp = response.json::<TokenResponse>().await.unwrap();
+        if let Err(_err) = response {
+            return Err(Error::new(ErrorKind::Other, "Error while fetching current user."))
+        }
 
-        let mut new_config = config.clone().borrow_mut().to_owned();
-        new_config.osu_access_token = resp.access_token.clone();
-        new_config.osu_refresh_token = resp.refresh_token.clone();
-        new_config.osu_token_expires_at = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + resp.expires_in;
+        let response = response.unwrap();
 
-        confy::store("mirria", None, new_config).expect("Error while saving config.");
+        let user = response.json::<UserResponse>().await;
 
-        future::ready(Ok(OsuClient { access_token: String::new(), refresh_token: String::new(), token_expires_at: 0 }))
+        if user.is_err() {
+            return Err(Error::new(ErrorKind::Other, "Looks like osu! servers down, or did html instead of json."))
+        }
+
+        let user = user.unwrap();
+        Ok(user)
     }
 
-    async fn from_tokens(config: Configuration, access_token: String, refresh_token: String) -> OsuClient {
-        
-        if config.osu_token_expires_at < SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() {
+    async fn from_tokens(
+        config: Configuration,
+        access_token: String,
+        refresh_token: String,
+    ) -> Result<OsuClient, Error> {
+        if config.osu_token_expires_at
+            < SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs()
+        {
             let mut client = OsuClient {
                 access_token: access_token,
                 refresh_token: refresh_token,
-                token_expires_at: config.osu_token_expires_at
+                token_expires_at: config.osu_token_expires_at,
             };
 
             let is_success = client.refresh_token(config.clone()).await;
-            
+
             if is_success.is_err() {
                 error!("Failed to refresh token");
+                return Err(Error::new(ErrorKind::Other, "Failed to refresh tokens"));
             }
 
-            return client;
+            let user = client.fetch_user().await;
+
+            if user.is_err() {
+                return Err(Error::new(ErrorKind::Other, "Failed to fetch user"));
+            }
+
+            let user = user.unwrap();
+
+            info!("Logged in as {} (https://osu.ppy.sh/users/{})", user.username, user.id);
+
+            return Ok(client);
         }
 
-        OsuClient {
+        //Validating tokens
+
+        let client = OsuClient {
             access_token: String::from(access_token),
             refresh_token: String::from(refresh_token),
             token_expires_at: config.osu_token_expires_at,
+        };
+
+
+        let user = client.fetch_user().await;
+
+        if user.is_err() {
+            return Err(Error::new(ErrorKind::Other, "Failed to fetch user"));
         }
+
+        let user = user.unwrap();
+
+        info!("Logged in as {} (https://osu.ppy.sh/users/{})", user.username, user.id);
+
+        Ok(client)
     }
 
     async fn refresh_token(&mut self, config: Configuration) -> Result<bool, Error> {
         let client = reqwest::Client::new();
-        
+
         let form = reqwest::multipart::Form::new()
-        .text("grant_type", "refresh_token")
-        .text("refresh_token", self.clone().refresh_token)
-        .text("client_id", "5")
-        .text("client_secret", "FGc9GAtyHzeQDshWP5Ah7dega8hJACAJpQtw6OXk")
-        .text("scope", "*");
+            .text("grant_type", "refresh_token")
+            .text("refresh_token", self.clone().refresh_token)
+            .text("client_id", "5")
+            .text("client_secret", "FGc9GAtyHzeQDshWP5Ah7dega8hJACAJpQtw6OXk")
+            .text("scope", "*");
 
         let response = client
             .post("https://osu.ppy.sh/oauth/token")
@@ -117,33 +210,53 @@ impl OsuApi for OsuClient {
         let mut new_config = config.clone().borrow_mut().to_owned();
         new_config.osu_access_token = resp.access_token.clone();
         new_config.osu_refresh_token = resp.refresh_token.clone();
-        new_config.osu_token_expires_at = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + resp.expires_in;
+        new_config.osu_token_expires_at = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            + resp.expires_in;
 
         confy::store("mirria", None, new_config).expect("Error while saving config.");
 
         self.access_token = resp.access_token;
         self.refresh_token = resp.refresh_token;
-        self.token_expires_at = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + resp.expires_in;
+        self.token_expires_at = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            + resp.expires_in;
 
         info!("Token refreshed.");
 
         Ok(true)
     }
 
-    async fn search_beatmapsets(&self, nsfw: bool, sort: String, status: String, cursor_string: Option<String>) -> Option<SearchResponse> {
+    async fn search_beatmapsets(
+        &self,
+        nsfw: bool,
+        sort: String,
+        status: String,
+        cursor_string: Option<String>,
+    ) -> Option<SearchResponse> {
         let client = reqwest::Client::new();
-      
-        let response = client.get("https://osu.ppy.sh/api/v2/beatmapsets/search")
-        .query(&[("nsfw", nsfw.to_string()), ("sort", sort), ("s", status), ("cursor_string", cursor_string.unwrap_or(String::new()))])
-        .bearer_auth(self.clone().access_token)
-        .send()
-        .await
-        .unwrap();
 
+        let response = client
+            .get("https://osu.ppy.sh/api/v2/beatmapsets/search")
+            .query(&[
+                ("nsfw", nsfw.to_string()),
+                ("sort", sort),
+                ("s", status),
+                ("cursor_string", cursor_string.unwrap_or(String::new())),
+            ])
+            .bearer_auth(self.clone().access_token)
+            .send()
+            .await
+            .unwrap();
 
         // let serialization_response = response.json::<SearchResponse>().await;
         let text = response.text().await.unwrap();
-        let jd: &mut serde_json::Deserializer<serde_json::de::StrRead<'_>> = &mut serde_json::Deserializer::from_str(text.as_str());
+        let jd: &mut serde_json::Deserializer<serde_json::de::StrRead<'_>> =
+            &mut serde_json::Deserializer::from_str(text.as_str());
 
         let result: Result<SearchResponse, _> = serde_path_to_error::deserialize(jd);
         match result {
@@ -151,43 +264,52 @@ impl OsuApi for OsuClient {
             Err(err) => {
                 let path = err.path().to_string();
                 error!("Failed to parse json, here path: {}", path);
-                return None
+                return None;
             }
         }
         // Some(serialization_response.unwrap())
     }
 
-    async fn download_if_not_exists(&self, id: String, path_to_beatmaps: String) -> Result<Vec<u8>, Error> {
-        
+    async fn download_if_not_exists(
+        &self,
+        id: String,
+        path_to_beatmaps: String,
+    ) -> Result<Vec<u8>, Error> {
         //Saving it to beatmaps folder
         let data_folder = Path::new(path_to_beatmaps.as_str());
         // let path_to_save = join_paths([data_folder, Path::new(format!("{}.osz", id).as_str())]).unwrap();
         let path_to_save = data_folder.join(format!("{}.osz", id));
         let file = File::open(path_to_save.clone()).await;
-        if file.is_ok(){
-            return Ok(Vec::new())
-        }else{
+        if file.is_ok() {
+            return Ok(Vec::new());
+        } else {
             info!("{:#?}", file.unwrap_err());
         }
         let client = reqwest::Client::new();
-      
-        let response = client.get(format!("https://osu.ppy.sh/api/v2/beatmapsets/{}/download", id))
-        .bearer_auth(self.clone().access_token)
-        .send()
-        .await
-        .unwrap();
+
+        let response = client
+            .get(format!(
+                "https://osu.ppy.sh/api/v2/beatmapsets/{}/download",
+                id
+            ))
+            .bearer_auth(self.clone().access_token)
+            .send()
+            .await
+            .unwrap();
 
         info!("got response");
 
         if !response.status().is_success() {
             let status = response.status().as_u16();
             error!("Invalid status: {}", status);
-            return Err(Error::new(ErrorKind::Other, "Error while downloading file."));
+            return Err(Error::new(
+                ErrorKind::Other,
+                "Error while downloading file.",
+            ));
         }
 
         let bytes = response.bytes().await.unwrap();
         //Saving it to data folder
-
 
         let result = write(path_to_save, bytes.clone());
         if result.is_err() {
@@ -195,5 +317,4 @@ impl OsuApi for OsuClient {
         }
         Ok(bytes.to_vec())
     }
-
 }
